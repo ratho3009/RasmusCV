eb8bea305703343f665b253e7dee8a8662290d65
ratho3009 <ratho3009@oslomet.no>
2026-06-04 14:11:56 +0200

    Added Header and start of Music page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://rasmusthorkildsen.cv',
+  integrations: [icon()],
 });
